0056c71dabbd240e3b74bda481b127c911e55eaf
colorbium <snbe225@gmail.com>
2017-05-16 15:04:57 -0500

    Change error

--- main.js
+++ main.js
@@ -49,33 +49,12 @@ function openFileSystem(){
     onFileSystemOpened(fs, true);
   });
 }
+
+
 //  window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
 //  window.requestFileSystem(window.PERSISTENT, 5*1024*1024, onInitFs, error);
 
 
 function error(e) {
-  var msg = '';
-
-  switch (e.code) {
-    case FileError.QUOTA_EXCEEDED_ERR:
-      msg = 'QUOTA_EXCEEDED_ERR';
-      break;
-    case FileError.NOT_FOUND_ERR:
-      msg = 'NOT_FOUND_ERR';
-      break;
-    case FileError.SECURITY_ERR:
-      msg = 'SECURITY_ERR';
-      break;
-    case FileError.INVALID_MODIFICATION_ERR:
-      msg = 'INVALID_MODIFICATION_ERR';
-      break;
-    case FileError.INVALID_STATE_ERR:
-      msg = 'INVALID_STATE_ERR';
-      break;
-    default:
-      msg = 'Unknown Error';
-      break;
-  };
-
-  console.log('Error: ' + msg);
+  console.log('Error Creating System');
 }
