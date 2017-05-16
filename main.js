081db0edb2aaca2e372e66ccb0d78631b4405261
colorbium <snbe225@gmail.com>
2017-05-16 16:30:52 -0500

    change js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-var supportsSyncFileSystem = chrome && chrome.syncFileSystem;
-
 function readFile(fs, fn) {
   fs.root.getFile(fn, {}, function(fileEntry) {
 
@@ -20,40 +18,50 @@ function readFile(fs, fn) {
   }, error);
 
 }
+function writeFile(fs, fn) {
+
+  fs.root.getFile(fn, {create: true}, function(fileEntry) {
+
+    // Create a FileWriter object for our FileEntry (log.txt).
+    fileEntry.createWriter(function(fileWriter) {
+
+      fileWriter.onwriteend = function(e) {
+        console.log('Write completed.');
+      };
+
+      fileWriter.onerror = function(e) {
+        console.log('Write failed: ' + e.toString());
+      };
+
+      // Create a new Blob and write it to log.txt.
+      var canvas = document.getElementById("page");
+
+      var blob = new Blob([canvas.toDataURL()], {type: 'image/jpg'});
+
+      fileWriter.write(blob);
+
+    }, error('error creating file'));
+
+  }, error('error creating file'));
+
+
 function onInitFs(fs) {
   console.log('File System Created')
-  //fs.root.getFile('log.txt', {create: true, exclusive: true}, function(fileEntry) {
-  //}, error);
+  fs.root.getFile('log.txt', {create: true, exclusive: true}, function(fileEntry) {
+  }, error('error creating file'));
 
 }
 function openFileSystem(){
-//  if (!chrome || !chrome.syncFileSystem ||
-//      !chrome.syncFileSystem.requestFileSystem) {
-//    error('Syncable FileSystem is not supported in your environment.');
-//    return;
-//  }
-
-  if (chrome.syncFileSystem.setConflictResolutionPolicy) {
-    chrome.syncFileSystem.setConflictResolutionPolicy('last_write_win');
-    show('#conflict-policy')
-  }
-  log('Obtaining syncable FileSystem...');
-  chrome.syncFileSystem.requestFileSystem(function (fs) {
-    if (chrome.runtime.lastError) {
-      error('requestFileSystem: ' + chrome.runtime.lastError.message);
-      $('#fs-syncable').classList.remove('selected');
-      hide('#conflict-policy')
-      return;
-    }
-    onFileSystemOpened(fs, true);
-  });
-}
-
-
+  var canvas = document.getElementById("page");
+  canvas.addEventListener("touchend", writeFile, false);
+  window.webkitStorageInfo.requestQuota(PERSISTENT, 1024*1024, function(grantedBytes) {
+  window.requestFileSystem(PERSISTENT, grantedBytes, onInitFs, error);
+}, error);
 //  window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
 //  window.requestFileSystem(window.PERSISTENT, 5*1024*1024, onInitFs, error);
-
+}
 
 function error(e) {
-  console.log('Error ' + e);
+
+  console.log('Error: ' + e);
 }
