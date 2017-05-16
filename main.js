4bc0825d71360c333c73d786fb5cbda88ec64f25
colorbium <snbe225@gmail.com>
2017-05-16 14:28:09 -0500

    Added Google Sample Code

---
+++ main.js
@@ -0,0 +1,63 @@
+var supportsSyncFileSystem = chrome && chrome.syncFileSystem;
+function readFile(fs, fn) {
+  fs.root.getFile(fn, {}, function(fileEntry) {
+
+    // Get a File object representing the file,
+    // then use FileReader to read its contents.
+    fileEntry.file(function(file) {
+       var reader = new FileReader();
+
+       reader.onloadend = function(e) {
+         var txtArea = document.createElement('textarea');
+         txtArea.value = this.result;
+         document.body.appendChild(txtArea);
+       };
+
+       reader.readAsText(file);
+    }, errorHandler);
+
+  }, errorHandler);
+
+}
+function onInitFs(fs) {
+  console.log('File System Created')
+  //fs.root.getFile('log.txt', {create: true, exclusive: true}, function(fileEntry) {
+  //}, error);
+
+}
+function openFileSystem(){
+  window.webkitStorageInfo.requestQuota(PERSISTENT, 1024*1024, function(grantedBytes) {
+  window.requestFileSystem(PERSISTENT, grantedBytes, onInitFs, errorHandler);
+}, function(e) {
+  console.log('Error', e);
+});
+//  window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
+//  window.requestFileSystem(window.PERSISTENT, 5*1024*1024, onInitFs, error);
+}
+
+function error(e) {
+  var msg = '';
+
+  switch (e.code) {
+    case FileError.QUOTA_EXCEEDED_ERR:
+      msg = 'QUOTA_EXCEEDED_ERR';
+      break;
+    case FileError.NOT_FOUND_ERR:
+      msg = 'NOT_FOUND_ERR';
+      break;
+    case FileError.SECURITY_ERR:
+      msg = 'SECURITY_ERR';
+      break;
+    case FileError.INVALID_MODIFICATION_ERR:
+      msg = 'INVALID_MODIFICATION_ERR';
+      break;
+    case FileError.INVALID_STATE_ERR:
+      msg = 'INVALID_STATE_ERR';
+      break;
+    default:
+      msg = 'Unknown Error';
+      break;
+  };
+
+  console.log('Error: ' + msg);
+}
