fef482de24d08d28ccebe8d37888d9ed7b506fe4
colorbium <snbe225@gmail.com>
2017-06-29 20:26:08 -0400

    fix syntax

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function dataURItoBlob(dataURI, callback){
     var byteString = atob(dataURI.split(',')[1]);
     var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
     var ab = new ArrayBuffer(byteString.length);
-    var ia - new Uint8Array(ab);
+    var ia = new Uint8Array(ab);
     for (var i =0; i<byteString.length;i++)
     {
 	ia[i] = byteString.charCodeAt(i);
