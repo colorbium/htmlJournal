1ccb92a48aede132c0ded0c07c8bfd956189ccc4
colorbium <snbe225@gmail.com>
2017-07-04 20:07:03 -0400

    first attempt at layers

--- filer.js
+++ filer.js
@@ -37,6 +37,9 @@ function onInitFs(fs) {
 //on next button click
 	//display next file
 function goNext(){
+	
+	//TODO-fix
+	//clear all canvas elements, etc
 	currimg=currimg+1;
 	var canvas = document.getElementById("page");canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
 	//check file exists
@@ -44,7 +47,7 @@ function goNext(){
   function(fileEntry) {//file exists
 		displayFile();}, 
   function(){//doesn't exist
-	writeFile();displayFile();
+	newFile();displayFile();
 	});
 	
 	
@@ -64,23 +67,157 @@ function goPrev(){
 
 //display file
 function displayFile() {
-	var fexists = false;
-	filesystem.root.getFile('page' + currimg.toString() + '.png', {create: false}, 
-  function(fileEntry) {//file exists
-	var canvas = document.getElementById("page");
-		var drawing = new Image();
-		drawing.src = "filesystem:"+window.location.origin +"/persistent/page" + (currimg).toString() + '.png'; // can also be a remote URL e.g. http://
-		drawing.onload = function() {
-			canvas.getContext("2d").drawImage(drawing,0,0);};
+	
+	//TODO fix
+	//open layers.txt
+	
+	fs.root.getDirectory(currDir, {create:false}, function(dirEntry){
+		fs.root.getFile('page' + currimg.toString() + '/layers.text', {create:false}, function(fileEntry)
+		{
+			function(fileEntry)
+			{//file exists
+				fileEntry.file(function(file){
+					var reader = new FileReader();
+					reader.onloadend = function(e){
+						//this.results
+						var text = e.target.result;
+						var lines = text.split(/[\r\n]+/g);
+						currLayer = lines.length + 1;
+						//for each line
+						for(i = 0; i<lines.length; i++)
+						{
+							//find canvas
+							var canvas = document.getElementById(lines[i]);
+							//display img
+							var drawing = new Image();
+							drawing.src = "filesystem:"+window.location.origin +"/persistent/page" + (currimg).toString() + '.png'; // can also be a remote URL e.g. http://
+							drawing.onload = function() {
+							canvas.getContext("2d").drawImage(drawing,0,0);};
 	
 	}, 
-  function(){writeFile();});
-
+						}
+				};
+			reader.readAsText(file, "UTF-8");
+			}, error);
+		});//end error callback
+		); //end get file
+  }, error); //end get Dir
 }
 
 //write file as image
 //additional function to create directory and save as layers 
 //for layer functionality
+function newFile() {
+  var fs = filesystem;
+  fs.root.getDirectory('page' + currimg.toString(), {create:true}, function(dirEntry){
+  newLayer('page' + currimg.toString());
+  }, error);
+}function addLayertoList(currLayer){
+			var list = document.getElementById('layerlist');
+			var li = document.createElement('li');
+			li.id=currLayer.toString();
+			list.appendChild(li);
+			li.innerHTML="Layer " + currLayer.toString();
+			var i = document.createElement('i');
+			i.className="js-remove";
+			i.innerHTML="X";
+			li.appendChild(i);
+}
+function updateLog(){
+var fs = filesystem;
+	fs.root.getDirectory(currDir, {create:false}, function(dirEntry){
+		fs.root.getFile('page' + currimg.toString() + '/layers.text', {create:true,exclusive:true}, function(fileEntry)
+		{
+				fileEntry.createWriter(function(fileWriter){
+						fileWriter.seek(fileWriter.length);
+						var blob = new Blob(['Layer1'], type:'text/plain'
+					},error);
+			}//end success callback
+		function(fileEntry)
+		{//file exists
+			fileEntry.createWriter(function(fileWriter){
+			var list = document.getElementById("layers".getElementsByTagName("li");
+				for(i = list.length-1; i=>0, i--)
+				{
+					var num = list[i].id;
+					var canvas = document.getElementById("canvas" + num.toString());
+					canvas.currentStyle["zIndex"] = i;
+				
+						var blob = new Blob([canvas.innerHTML], type:'text/plain');
+					}
+					},error);
+
+		});//end error callback
+		); //end get file
+  }, error); //end get Dir
+}
+function newLayer(currDir)
+{
+	var fs = filesystem;
+	//new element in list
+	//new canvas element
+	//update z-indexes
+	//update layers.txt
+	var currLayer=1;
+	fs.root.getDirectory(currDir, {create:false}, function(dirEntry){
+		fs.root.getFile('page' + currimg.toString() + '/layers.text', {create:true,exclusive:true}, function(fileEntry)
+		{
+				fileEntry.createWriter(function(fileWriter){
+						fileWriter.seek(fileWriter.length);
+						var blob = new Blob(['canvas'+currLayer.toString()], type:'text/plain');
+					},error);
+			}//end success callback
+		function(fileEntry)
+		{//file exists
+			fileEntry.file(function(file){
+				var reader = new FileReader();
+				reader.onloadend = function(e){
+					//this.results
+					var text = e.target.result;
+					var lines = text.split(/[\r\n]+/g);
+					currLayer = lines.length + 1;
+					fileEntry.createWriter(function(fileWriter){
+						fileWriter.seek(fileWriter.length);
+						var blob = new Blob(['canvas'+currLayer.toString()], type:'text/plain'
+					},error);
+				};
+			reader.readAsText(file, "UTF-8");
+			}, error);
+		});//end error callback
+		); //end get file
+  }, error); //end get Dir
+  //new element in list
+  addLayertoList(currLayer);
+	//new canvas element
+  var newc = document.createElement("canvas");
+  newc.id = "canvas" + currLayer.toString();
+  newc.className="selected";
+	//update z-indexes
+  var z = 0;
+  var not = document.getElementsByClassName("not");
+  var i;
+  for (i=0; i <not.length; i++){
+	if(slt[i].currentStyle["zIndex"] > z)
+		{
+			z = slt[i].currentStyle["zIndex"];
+		}
+  }
+  var slt = document.getElementsByClassName("selected");
+  for (i=0; i <slt.length; i++){
+	slt[i].className = "not";
+	if(slt[i].currentStyle["zIndex"] > z)
+	{
+		z = slt[i].currentStyle["zIndex"];
+	}
+	newc.currentStyle["zIndex"] = z+1;
+	var cdiv = document.getElementById("page");
+	cdiv.appendChild(cdiv);
+  }
+}
+function error(e) {
+
+  console.log('Error: ' + e);
+}
 function writeFile() {
   var fs = filesystem;
   fs.root.getFile('page' + currimg.toString() + '.png', {create: true, exclusive:false}, function(fileEntry) {
@@ -97,8 +234,8 @@ function writeFile() {
       };
 
       // Create a new Blob and write it to log.txt.
-      var canvas = document.getElementById("page");
-
+     // var canvas = document.getElementById("page");
+	  var canvas = document.getElementsByClassName("selected")[0];
       var blob = new Blob([canvas.toDataURL()], {type: 'image/jpg'});
 
       fileWriter.write(dataURItoBlob(canvas.toDataURL("image/png")));
@@ -107,12 +244,6 @@ function writeFile() {
 
   }, error('2'));
 }
-
-function error(e) {
-
-  console.log('Error: ' + e);
-}
-
 //helper to make blob
 function dataURItoBlob(dataURI, callback){
     var byteString = atob(dataURI.split(',')[1]);
